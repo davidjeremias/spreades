9c6f6e6d750a219caba16dc9d3c2b9e15a8e4522
David Jeremias Souza do Carmo <p577394@corp.caixa.gov.br>
2018-11-20 15:56:49 -0200

    melhorias no css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  lbl: string = "Nome:";
+  lbl: string = "Email:";
 
   ngOnInit(){
     console.log(this.lbl);
